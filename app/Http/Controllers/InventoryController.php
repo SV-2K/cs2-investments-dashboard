a0a01049a66aad5b870ab47dfc235a97ef3f4626
SV2K <savva7446@gmail.com>
2025-11-15 21:21:12 +0300

    Add inventory checker

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -9,6 +9,32 @@ class InventoryController extends Controller
 {
     public function index()
     {
-        return view('test');
+        $steamId64 = auth()->user()->steamid64;
+        $url = "https://steamcommunity.com/inventory/{$steamId64}/730/2?l=english&count=2000";
+
+        $response = Http::withHeaders([
+            'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120 Safari/537.36',
+            'Accept' => 'application/json, text/javascript, */*; q=0.01',
+            'Referer' => "https://steamcommunity.com/profiles/{$steamId64}/inventory",
+        ])->get($url)->json();
+
+//        dd($response);
+
+        $items = [];
+        foreach ($response['assets'] as $asset) {
+            foreach ($response['descriptions'] as $description) {
+                if ($description['classid'] === $asset['classid']) {
+                    $items[$asset['assetid']] = [
+                        'classid' => $asset['classid'],
+                        'icon_url' => $description['icon_url'],
+                        'name' => $description['market_name'],
+                    ];
+                }
+            }
+        }
+
+        ksort($items);
+
+        return view('test', compact('items'));
     }
 }
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,2 +1,5 @@
 {{ auth()->user()->name }}
-
+@dump(count($items))
+@foreach($items as $item)
+    <img width="100" height="100" src="https://community.fastly.steamstatic.com/economy/image/{{ $item['icon_url'] }}">
+@endforeach
